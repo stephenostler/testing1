73f065415b654ced19d179f55565bd990fd361c7
StephenOstler <stephen.ostler0120@stu.mtec.edu>
2023-11-29 13:50:32 -0700

    first files for commit

---
+++ positivepath/pageobjects/page.js
@@ -0,0 +1,9 @@
+
+const { browser } = require('@wdio/globals')
+
+module.exports = class Page {
+   
+    open (path) {
+        return browser.url(`https://www.saucedemo.com/`)
+    }
+}
---
+++ positivepath/specs/test.js
@@ -0,0 +1,13 @@
+const { expect } = require('@wdio/globals')
+const LoginPage = require('../pageobjects/login.page')
+const loginPage = require('../pageobjects/login.page')
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+
+        await LoginPage.login('standard_user', 'secret_sauce')
+        await expect(loginPage.pagecheck).toBeExisting()
+    })
+})
+
